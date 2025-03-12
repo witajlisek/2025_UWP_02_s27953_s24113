7cb5974db1eb880bd5677ff98eb08860129d9f95
Jakub Liszewski <153238469+witajlisek@users.noreply.github.com>
2025-03-12 10:56:55 +0100

    Update EnemySO.cs

--- Assets/Scripts/Enemy/EnemySO.cs
+++ Assets/Scripts/Enemy/EnemySO.cs
@@ -1,14 +1,9 @@
 using UnityEngine;
+
 [CreateAssetMenu(menuName="Enemy/New Enemy")]
 namespace Enemy{
-		public class EnemySO : ScriptableObject
-{
-public string enemyName;
-
-
-
-
-
-public int damage;
+	public class EnemySO : ScriptableObject {
+	public string enemyName;
+	public int damage;
 	}
-		}
+}
